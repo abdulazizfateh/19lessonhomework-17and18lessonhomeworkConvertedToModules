e7f6e7159e45558b332aa308143bd54104eb5a98
Abdulaziz Ergashev <eabdulaziz36@gmail.com>
2024-08-27 05:33:49 +0500

    17&18lessonhomework are converted to Modules

--- main.js
+++ main.js
@@ -4,15 +4,15 @@
 // 1.3 main.js da hammasini execute qilinglar 
 
 // Functions in Module Execution
-// import { isPrime } from "./1functions.js";
+// import { isPrime } from "./17.1functions.js";
 // console.log(isPrime(11));
 
 // Data (Array(Object)) in Module Execution
-// import { arr } from "./1data.js";
+// import { arr } from "./17.1data.js";
 // console.log(arr);
 
 // Constructor Function in Module Execution
-// import { Abdulaziz, Muhammadjon } from "./1constfunctions.js";
+// import { Abdulaziz, Muhammadjon } from "./17.1constfunctions.js";
 // console.log(Abdulaziz);
 // console.log(Muhammadjon);
 
@@ -26,7 +26,8 @@
 
 // TASK 2 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 // 2. "abcd" => "a-bb-ccc-dddd" 
-// import { pickTheElement } from "./2task.js";
+
+// import { pickTheElement } from "./17.2task.js";
 // console.log(pickTheElement('abcd'));
 
 
@@ -40,8 +41,8 @@
 // TASK 3 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 // 3. palindrom string 
 
-// import { checkThePalindrome } from "./3task.js";
-// console.log(checkThePalindrome('aziza'));
+// import { checkThePalindrome } from "./17.3task.js";
+// console.log(checkThePalindrome('Aziza'));
 
 
 
@@ -54,7 +55,7 @@
 // TASK 4 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 // 4. string harflari qaytarilmagan bo'lsa true yoki false 
 
-// import { checkTheDuplicate } from "./4task.js";
+// import { checkTheDuplicate } from "./17.4task.js";
 // console.log(checkTheDuplicate('Murod'));
 
 
@@ -66,9 +67,9 @@
 
 
 // TASK 5 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-// 5. arrey ichidagi raqamar yeg’indisi juft yoki toq 
+// 5. arrey ichidagi raqamlar yeg’indisi juft yoki toq 
 
-// import { findSum } from "./5task.js";
+// import { findSum } from "./17.5task.js";
 // console.log(findSum([1, 2, 3, 4, 5, 6, 7]));
 
 
@@ -82,5 +83,5 @@
 // TASK 6 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 // 6. raqam beriladi shu raqam bo’luvchilarini bitta arreyga solib qaytarsin tub bo’lsa tub so’zini
 
-// import { findTheDivisors } from "./6task.js";
+// import { findTheDivisors } from "./17.6task.js";
 // console.log(findTheDivisors(20));
